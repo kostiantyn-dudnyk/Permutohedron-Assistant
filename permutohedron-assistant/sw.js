48b3b253c36284c0448a5a59f504702ca8f2b6c7
Kostya-404 <92384427+Kostya-404@users.noreply.github.com>
2025-11-22 11:30:25 +0200

    Delete permutohedron-assistant directory

--- permutohedron-assistant/sw.js
+++
@@ -1,4 +0,0 @@
-const CACHE = 'perm-v1';
-const urls = ['/', '/index.html', '/style.css', '/script.js', '/manifest.json'];
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(urls))));
-self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
